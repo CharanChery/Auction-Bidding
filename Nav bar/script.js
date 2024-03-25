6abdb7d473f463ef62ee74442503de2325343041
CharanChery <charanchery70@gmail.com>
2024-03-25 21:57:57 +0530

    nav bar

--- Nav bar/script.js
+++ Nav bar/script.js
@@ -1,7 +1,9 @@
 let menu = document.querySelector(".menu-icon");
 let navlist = document.querySelector(".navlist");
+let searchbar = document.querySelector(".searchbar");
 
 menu.onclick = () =>{
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
+    searchbar.classList.toggle('open');
 }
